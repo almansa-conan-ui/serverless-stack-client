b4331241dc29eef4cde882425ad8a3fbe7651023
almansa@conan-ui.com <almansa@conan-ui.com>
2020-08-12 18:07:49 +0200

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+        REGION: "eu-west-2",
+        BUCKET: "notes-app-uploads-aalmansa"
+    },
+    apiGateway: {
+        REGION: "eu-west-2",
+        URL: "https://zo4l6y8kuj.execute-api.eu-west-2.amazonaws.com/prod/"
+    },
+    cognito: {
+        REGION: "eu-west-2",
+        USER_POOL_ID: "eu-west-2_4RYI1rPxe",
+        APP_CLIENT_ID: "qpfnnfoos7c9j5733065qa1eq",
+        IDENTITY_POOL_ID: "eu-west-2:5245f2b9-6c9b-41af-b758-9f6b314025aa"
+    }
+};
